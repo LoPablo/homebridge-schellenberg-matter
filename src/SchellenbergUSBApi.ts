495ed260ad719c19042700ee79d2d8923f6f2d05
lopablo <lopablo@proton.me>
2026-05-29 22:11:33 +0200

    Changed Delimiter

--- src/SchellenbergUSBApi.ts
+++ src/SchellenbergUSBApi.ts
@@ -38,7 +38,7 @@ export class SchellenbergUSBApi {
             baudRate: 115200
         })
         this.serialPort.write('helo')
-        this.parser = this.serialPort.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+        this.parser = this.serialPort.pipe(new ReadlineParser({ delimiter: '\n' }))
         this.serialPort.on('data', (data) => {
             this.log.info('Received data', data)
         })
